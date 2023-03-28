13974aa763af7132a57de844b181a2b4c74a7306
aminulis107 <aminulis107@gmail.com>
2023-03-28 15:58:10 +0600

    admin page show again

--- webtool/first_app/urls.py
+++ webtool/first_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('compress/', views.compress, name='compress'),
     path('convert/', views.convert, name='convert'),
     path('merge/', views.merge, name='merge'),
+    
    
 ]
--- webtool/webtool/settings.py
+++ webtool/webtool/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "first_app.urls"
+ROOT_URLCONF = "webtool.urls"
 
 TEMPLATES = [
     {
@@ -115,10 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
+STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
